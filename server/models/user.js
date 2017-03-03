e21cebc80e1bc080ae86fe8c434924c26aec1b53
Arya Viciyudha <arya.rezavidi@gmail.com>
2017-03-03 23:53:43 +0700

    Add GET /users/me and authentication

--- server/models/user.js
+++ server/models/user.js
@@ -1,22 +1,75 @@
 var mongoose = require('mongoose');
+var validator = require('validator');
+var jwt = require('jsonwebtoken');
+var _  = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
     minlentgh: 1,
-  }
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
-module.exports = {User};
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
 
-// var user = new User({
-//   email: 'arya.rezavidi@gmail.com'
-// });
-//
-// user.save().then((doc) => {
-//   console.log(doc);
-// }, (e) => {
-//   console.log(e);
-// });
+  return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123');
+
+  user.tokens.push({access, token});
+  return user.save().then(() => {
+    return token;
+  });
+}
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decode;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+}
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -8,13 +8,35 @@ var {mongoose} = require('./db/mongoose');
 var {ObjectID} = require('mongodb');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {authenticate} = require('./middleware/authenticate');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 var app = express();
 
 
 app.use(bodyParser.json());
+
+// POST User
+app.post('/users', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+    //res.send(user);
+  }).then((token) => {
+    console.log(token);
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+// GET User/me
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
+});
 
 // GET Todos
 app.get('/todos', (req, res) => {
